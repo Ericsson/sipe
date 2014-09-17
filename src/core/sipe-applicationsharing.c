c7c8e7655670787a3432c5eb5bcf4c2b02e5249d
Jakub Adam <jakub.adam@ktknet.cz>
2014-09-17 18:24:06 +0200

    appshare: successful incoming share

--- src/core/sipe-applicationsharing.c
+++ src/core/sipe-applicationsharing.c
@@ -21,60 +21,113 @@
  */
 
 #include <glib.h>
+#include <glib/gstdio.h>
+#include <gio/gio.h>
+#include <gio/gunixsocketaddress.h>
+
+#include <stdlib.h>
 
 #include "sipe-applicationsharing.h"
 #include "sipe-backend.h"
+#include "sipe-common.h"
 #include "sipe-core.h"
 #include "sipe-core-private.h"
 #include "sipe-media.h"
+
+struct {
+	struct sipe_backend_media *media;
+	struct sipe_backend_stream *stream;
+	GSocket *socket;
+	GSocket *data_socket;
+	GIOChannel *listen_channel;
+	GIOChannel *data_channel;
+} foobar;
+
+static void
+hexdump (const gchar *str, gsize len) {
+	const gchar *data = (const gchar *) str;
+	guint n, m;
+	for (n = 0; n < len; n += 16) {
+		g_printerr ("%04x: ", n);
+		for (m = n; m < n + 16; m++) {
+			if (m > n && (m%4) == 0)
+				g_printerr (" ");
+			if (m < len)
+				g_printerr ("%02x ", data[m]);
+			else
+				g_printerr (" ");
+		}
+		g_printerr (" ");
+		for (m = n; m < len && m < n + 16; m++)
+			g_printerr ("%c", g_ascii_isprint (data[m]) ? data[m] : '.');
+		g_printerr ("\n");
+	}
+}
 
 static void
 read_cb(struct sipe_media_call *call, struct sipe_backend_stream *backend_stream)
 {
 	guint8 buffer[0x800];
+	gint bytes_read;
+	gsize bytes_written;
+	GError *error = 0;
 
 	SIPE_DEBUG_INFO_NOFORMAT("INCOMING APPSHARE DATA");
-	sipe_backend_media_read(call->backend_private,
+	bytes_read = sipe_backend_media_read(call->backend_private,
 			backend_stream, buffer, sizeof (buffer), FALSE);
+
+	if (bytes_read == 0) {
+		return;
+	}
+
+	hexdump((gchar *)buffer, bytes_read);
+	g_io_channel_write_chars(foobar.data_channel, (gchar *)buffer, bytes_read, &bytes_written, &error);
+	g_assert_no_error(error);
+	g_io_channel_flush(foobar.data_channel, &error);
+	g_assert_no_error(error);
+	g_assert(bytes_read == (gint)bytes_written);
 }
-
-static void
-_send_handshake(struct sipe_backend_media *media,
-		struct sipe_backend_stream *stream)
-{
-      guint8 handshake[] = {0x03, 0x00, 0x00, 0x2f, 0x2a, 0xe0, 0x00, 0x00,
-                            0x00, 0x00, 0x00, 0x43, 0x6f, 0x6f, 0x6b, 0x69,
-                            0x65, 0x3a, 0x20, 0x6d, 0x73, 0x74, 0x73, 0x68,
-                            0x61, 0x73, 0x68, 0x3d, 0x73, 0x69, 0x70, 0x3a,
-                            0x6d, 0x61, 0x72, 0x63, 0x2d, 0x0d, 0x0a, 0x01,
-                            0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00};
-      //guint8 buffer[2048];
-      //gint len;
-      g_debug("*************** SENDING RDP HANDSHAKE ********************");
-      sipe_backend_media_write(media, stream, handshake, sizeof(handshake), TRUE);
-
-     /* g_debug("*************** Reading response ********************");
-
-      len = sipe_backend_media_read(media, stream, buffer, sizeof(buffer), FALSE);
-
-      g_debug("Read %d bytes", len);
-      len = sipe_backend_media_read(media, stream, buffer, 0x10, TRUE);
-      g_debug("Read %d bytes", len);
-      len = sipe_backend_media_read(media, stream, buffer, sizeof(buffer), FALSE);
-
-      g_debug("Read %d bytes", len);*/
-}
-
-struct {
-	struct sipe_backend_media *media;
-	struct sipe_backend_stream *stream;
-} foobar;
 
 static gboolean
-_send_handshake_cb(gpointer user_data)
+data_in_cb (SIPE_UNUSED_PARAMETER GIOChannel *channel,
+	    SIPE_UNUSED_PARAMETER GIOCondition condition,
+	    SIPE_UNUSED_PARAMETER gpointer data)
 {
-	(void)user_data;
-	_send_handshake(foobar.media, foobar.stream);
+	GError *error = NULL;
+	gchar buffer[2048];
+	gsize bytes_read;
+
+	while (1) {
+		g_io_channel_read_chars(channel, buffer, sizeof (buffer), &bytes_read, &error);
+		g_assert_no_error(error);
+
+		if (bytes_read > 0) {
+			hexdump(buffer, bytes_read);
+			sipe_backend_media_write(foobar.media, foobar.stream, (guint8 *)buffer, bytes_read, TRUE);
+		} else {
+			break;
+		}
+	}
+
+	return TRUE;
+}
+
+static gboolean
+socket_connect_cb (SIPE_UNUSED_PARAMETER GIOChannel *channel,
+		   SIPE_UNUSED_PARAMETER GIOCondition condition,
+		   SIPE_UNUSED_PARAMETER gpointer data)
+{
+	GError *error = NULL;
+
+	foobar.data_socket = g_socket_accept(foobar.socket, NULL, &error);
+	g_assert_no_error(error);
+	g_io_channel_shutdown(channel, TRUE, &error);
+
+	foobar.data_channel = g_io_channel_unix_new(g_socket_get_fd(foobar.data_socket));
+	g_io_channel_set_encoding(foobar.data_channel, NULL, &error);
+	g_assert_no_error(error);
+	g_io_add_watch(foobar.data_channel, G_IO_IN , data_in_cb, NULL);
+
 	return FALSE;
 }
 
@@ -83,11 +136,33 @@ writable_cb(struct sipe_media_call *call,
 	    struct sipe_backend_stream *backend_stream,
 	    gboolean writable)
 {
-	if (writable) {
+	if (writable && !foobar.socket) {
+		gchar socket_path[] = "/tmp/sipe-appshare-socket";
+		GSocketAddress *address;
+		GError *error = NULL;
+
+		foobar.socket = g_socket_new(G_SOCKET_FAMILY_UNIX,
+					     G_SOCKET_TYPE_STREAM,
+					     G_SOCKET_PROTOCOL_DEFAULT,
+					     &error);
+		g_assert_no_error(error);
+		g_socket_set_blocking(foobar.socket, FALSE);
+
+		address = g_unix_socket_address_new(socket_path);
+
+		g_unlink(socket_path);
+
+		g_socket_bind(foobar.socket, address, TRUE, &error);
+		g_assert_no_error(error);
+		g_socket_listen(foobar.socket, &error);
+		g_assert_no_error(error);
+
+		foobar.listen_channel = g_io_channel_unix_new(g_socket_get_fd(foobar.socket));
+		g_io_add_watch(foobar.listen_channel, G_IO_IN, socket_connect_cb, NULL);
+
 		/* We need to send the data after the reinvite, or need to set the encryption params after the first invite*/
 		foobar.media = call->backend_private;
 		foobar.stream = backend_stream;
-		g_timeout_add(1000, _send_handshake_cb, NULL);
 	}
 }
 
@@ -98,6 +173,8 @@ process_incoming_invite_applicationsharing(struct sipe_core_private *sipe_privat
 	struct sipe_media_call *call = NULL;
 
 	process_incoming_invite_call(sipe_private, msg);
+
+	foobar.socket = 0;
 
 	call = (struct sipe_media_call *)sipe_private->media_call;
 	if (call) {
