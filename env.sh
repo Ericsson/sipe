7b8d241d94827ff26a9d151166e9013c8582078b
Muthu Subramanian <muthu.subramanian.karunanidhi@ericsson.com>
2015-02-19 18:03:23 +0530

    Add Environment setup helper

---
+++ env.sh
@@ -0,0 +1,8 @@
+export PURPLE_CFLAGS=-I/usr/include/libpurple\ -I/usr/include/nice\ -I/opt/gstreamer1.4/include/gstreamer-1.0\ -I/usr/include/libxml2
+export PURPLE_LIBS=-L/usr/lib/purple-3
+
+export LD_LIBRARY_PATH=/opt/gstreamer1.4/lib/x86_64-linux-gnu:/usr/local/lib/x86_64-linux-gnu
+export PKG_CONFIG_PATH=/opt/gstreamer1.4/lib/x86_64-linux-gnu/pkgconfig
+
+export FREERDP_LIBS=-lfreerdp-shadow\ -L/usr/local/lib/x86_64-linux-gnu
+export FREERDP_CFLAGS=-I/usr/local/include/freerdp
