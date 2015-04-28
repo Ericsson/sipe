1e960fb6612bb3e4386c915d4c8db00d4e195365
Aparajita <aparajita.mohapatra@ericsson.com>
2015-04-28 09:09:44 +0530

    Screen sharing change

--- src/core/sipe-applicationsharing.h
+++ src/core/sipe-applicationsharing.h
@@ -39,10 +39,4 @@ struct sip_session * sipe_session_find_call(struct sipe_core_private *sipe_priva
 void process_incoming_invite_applicationsharing(struct sipe_core_private *sipe_private,
 						struct sipmsg *msg);
 
-//void accept_sharing(gchar* sharing_path);
-
-//void add_user(struct monitor_sharing** list, const gchar* new_user,rdpShadowServer* server);
-
-//void delete_user(struct monitor_sharing **list, const gchar *user_name);
-
 gboolean is_screen_shared(const gchar* user_name);
